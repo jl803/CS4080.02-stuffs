6180cbac2bcdd6ba29ffd83e87935012f05cb70c
jonahlin803 <168327681+jonahlin803@users.noreply.github.com>
2026-09-17 16:03:28 -0700

    HW 3

---
+++ HW 3/Ch6Q3/Lox.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Lox {
+  private static boolean hadError = false;
+
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: java Lox [script]");
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+
+  private static void runFile(String fileName) throws IOException {
+    String source = Files.readString(Path.of(fileName), StandardCharsets.UTF_8);
+    run(source);
+
+    if (hadError) {
+      System.exit(65);
+    }
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    while (true) {
+      System.out.print("> ");
+      String line = reader.readLine();
+
+      if (line == null) {
+        break;
+      }
+
+      run(line);
+      hadError = false;
+    }
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+
+    if (hadError) {
+      return;
+    }
+
+    System.out.println(new AstPrinter().print(expression));
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
+  }
+
+  private static void report(int line, String location, String message) {
+    System.err.println("[line " + line + "] Error" + location + ": " + message);
+    hadError = true;
+  }
+}
